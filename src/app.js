fba3609171a5257e6e407eea1ee9bc7171b0024b
輕翼 <42484761+qingyiwebt@users.noreply.github.com>
2021-09-04 23:33:13 +0900

    make inputbox bigger

--- src/app.js
+++ src/app.js
@@ -164,7 +164,7 @@ class App{
         const getBtnGroups = (name, min, max)=>{
             const group = $(`<li>${name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>`);
             const btnSub = $(`<span class="iconfont propbtn">&#xe6a5;</span>`);
-            const inputBox = $(`<input value="0">`);
+            const inputBox = $(`<input value="0" style="width: 3rem">`);
             const btnAdd = $(`<span class="iconfont propbtn">&#xe6a6;</span>`);
             group.append(btnSub);
             group.append(inputBox);
